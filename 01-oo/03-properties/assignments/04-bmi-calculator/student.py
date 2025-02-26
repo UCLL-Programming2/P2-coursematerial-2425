9d45167552b4518653010f375b900043d1ba55e8
r0935730 <timodeclerck29@gmail.com>
2025-02-26 23:46:41 +0100

    Update student.py

--- 01-oo/03-properties/assignments/04-bmi-calculator/student.py
+++ 01-oo/03-properties/assignments/04-bmi-calculator/student.py
@@ -1,1 +1,20 @@
+class BMICalculator:
+    def __init__(self, weight_in_kg, height_in_m):
+        self.weight_in_kg = weight_in_kg
+        self.height_in_m = height_in_m
+
+    @property
+    def bmi(self):
+       bmi = self.weight_in_kg / self.height_in_m ** 2
+       return bmi
+    @property
+    def category(self):
+        bmi = self.weight_in_kg / self.height_in_m ** 2
+        if bmi < 18.5:
+            return "underweight"
+        elif bmi > 18.5 and bmi < 25:
+            return "normal"
+        elif bmi > 25:
+            return "overweight"
+
 # Write your code here
